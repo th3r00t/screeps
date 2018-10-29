6fcdec1cd7bae1e2d865e032c965d402de038f7e
th3r00t <th3r00t@users.noreply.github.com>
2018-10-29 14:39:25 -0400

    Refactored functions, and added a try catch block

--- maint.creeps.js
+++ maint.creeps.js
@@ -1,23 +1,27 @@
 var maintcreeps = {
     run: function(creep){
         var c_count = 0   
-        for (i in Game.creeps){
-            c_count++
-        }
-        if(c_count == 0)  {var init = true}
+        for (i in Game.creeps){c_count++}
+        if (c_count == 0)  {var init = true}
         if (init == true || c_count < 6){
             var creep_num = c_count + 1;
             if(c_count < 2 || c_count == null){
                 Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester'+creep_num );
-                Game.creeps['Harvester'+creep_num].memory.role = 'harvester'
+                try {Game.creeps['Harvester'+creep_num].memory.role = 'harvester'} catch (error) {
+                    return;
+                }          
             }
-            else if (c_count >= 2 && c_count < 4){
+            else if (c_count >= 2 && c_count < 4){  
                 Game.spawns['Spawn1'].spawnCreep( [WORK,CARRY,MOVE], 'Upgrader'+creep_num);
-                Game.creeps['Upgrader'+creep_num].memory.role = 'upgrader'
+                try {Game.creeps['Upgrader'+creep_num].memory.role = 'upgrader'} catch (error) {
+                    return;
+                }               
             }
             else if (c_count >= 4){
                 Game.spawns['Spawn1'].spawnCreep( [WORK,CARRY,MOVE,MOVE], 'Builder'+creep_num);
-                Game.creeps['Builder'+creep_num].memory.role = 'builder'
+                try {Game.creeps['Builder'+creep_num].memory.role = 'builder'} catch (error) {
+                    return;
+                }                
             }
         }
         else{/*All creeps spawned*/}
